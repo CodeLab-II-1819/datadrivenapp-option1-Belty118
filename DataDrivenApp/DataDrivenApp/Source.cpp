7413e544a46c1291e902bacd96be7e76458377c2
Belty118 <44161197+Belty118@users.noreply.github.com>
2019-01-30 03:41:00 +0000

    Made some changes

--- DataDrivenApp/DataDrivenApp/Source.cpp
+++ DataDrivenApp/DataDrivenApp/Source.cpp
@@ -4,28 +4,28 @@
 #include <string>
 #include <sstream>
 #include <fstream>
+#include <cstdlib> 
 using namespace std;
 
 void count();
-void money();
-void politics();
-void paris();
-void dreamworks();
-void uber();
-void search();
+void menuchoice(string);
+
 
 int menu1;
+int menu2;
 string myData;
 ifstream inFile;
 int numberOfTweets = 0;
 string userInput;
+string choice;
+int choicenum;
 
 
 int main() {
 
 	inFile.open("Sampletweets.txt");
 
-	cout << "Please press the key, for the option you wish to see.\n\nOption 1: Count all tweets in file. \nOption 2: Print all tweetscontaining the word 'money'\nOption 3: Print all tweets containing the word 'Politics'\nOption 4: Print all tweets containing the word'paris'\nOption 5: Print all tweets containing the word'dreamworks'\nOption 6: Print all tweets containing the word 'uber'\nOption 7: Print all tweets with a word of your choice \n";
+	cout << "Please press the key, for the option you wish to see.\n\nOption 1: Count all tweets in file. \nOption 2: Print all tweets containing the word 'money'\nOption 3: Print all tweets containing the word 'politics'\nOption 4: Print all tweets containing the word 'Paris'\nOption 5: Print all tweets containing the word 'Dreamworks'\nOption 6: Print all tweets containing the word 'Uber'\nOption 7: Print all tweets with a word of your choice \nOption 8: Print all Retweets\n";
 	cin >> menu1;
 
 	switch (menu1) {
@@ -34,35 +34,35 @@ int main() {
 		count();
 		break;
 	case 2: cout << "\nYou have chosen option: " << menu1 << " Counting tweets containing the word 'money'" << endl;
-		money();
+		menuchoice("money");
 		break;
 	case 3: cout << "\nYou have chosen option: " << menu1 << " Counting the number of tweets that mention politics" << endl;
-		politics();
+		menuchoice("politics");
 		break;
 	case 4: cout << "\nYou have chosen option: " << menu1 << " Printing all tweets that contain the word 'Paris'" << endl;
-		paris();
+		menuchoice("Paris");
 		break;
 	case 5: cout << "\nYou have chosen option: " << menu1 << " Printing all tweets that contain the word 'Dreamworks'" << endl;
-		dreamworks();
+		menuchoice("Dreamworks");
 		break;
 	case 6: cout << "\nYou have chosen option: " << menu1 << " Printing all tweets that contain the word 'Uber'" << endl;
-		uber();
+		menuchoice("Uber");
 		break;
 	case 7: cout << "\nYou have chosen option: " << menu1 << " Please enter the word you want to search for in the tweets" << endl;
 		cin >> userInput;
-		search();
-	case 8: cout << "" << endl;
+		menuchoice(userInput);
+	case 8: cout << "\nYou have chosen option:" << menu1 << " Printing all retweets" << endl;
+		menuchoice("RT");
+		break;
+	case 10: cout << "" << endl;
 		break;
 	default: cout << "Please press the key for the option you wish to see" << endl;
 
 	}
-
-	return 0;
-
 }
+	
 
 void count() {
-
 
 	if (inFile.good()) { //check file is opened
 		while (!inFile.eof()) { // while you're not at the end of the file
@@ -72,92 +72,53 @@ void count() {
 
 		cout << "There are: " << numberOfTweets << " Tweets in the file." << endl;
 		inFile.close(); // close the file
+
+		cout << "Return to the main menu please press 1 or to exit, please press 2" << endl;
+		cin >> menu2;
+
+			if (menu2 == 1){
+				main();
+			}
+
+			else if (menu2 == 2) {
+				exit(0);
+			}
+
+		system("pause");
+
+	}
+}
+
+
+
+void menuchoice(string choice) {
+
+	if (inFile.good()) {
+		while (!inFile.eof()) {
+			getline(inFile, myData);
+
+			if (myData.find(" " + choice + " ") < myData.length()) {
+				choicenum++;
+				cout << myData << endl;
+			}
+
+		}
+		cout << "There are: " << choicenum << " Tweets containing the word " << choice << endl;
+		inFile.close();
+
+		cout << "Return to the main menu please press 1 or to exit, please press 2" << endl;
+		cin >> menu2;
+
+		if (menu2 == 1) {
+			main();
+		}
+
+		else if (menu2 == 2) {
+			exit(0);
+		}
+
+		system("pause");
 	}
 
 
-};
-
-void money() {
-
-
-	if (inFile.good()) {         //if the file is opened properly
-		while (!inFile.eof()) {             //while you're not at the end of the file
-			getline(inFile, myData);             //get the line and put the data into a string
-
-			if (myData.find("money") < myData.length() || myData.find("Money") < myData.length()) {   //if the string you collected above has the word you're looking for in it
-				cout << myData << endl;    //print the data
-			}
-		}
-		inFile.close();        //close the file when done
-
-	};
-};
-
-void politics() {
-
-};
-
-void paris() {
-
-	if (inFile.good()) {
-		while (!inFile.eof()) {
-			getline(inFile, myData);
-
-			if (myData.find("paris") < myData.length() || myData.find("Paris") < myData.length()) {
-				cout << myData << endl;
-			}
-		}
-		inFile.close();
-
-	};
-
-};
-
-void dreamworks() {
-
-	if (inFile.good()) {
-		while (!inFile.eof()) {
-			getline(inFile, myData);
-
-			if (myData.find("dreamworks") < myData.length() || myData.find("Dreamworks") < myData.length()) {
-				cout << myData << endl;
-			}
-		}
-		inFile.close();
-
-	};
-
-};
-
-void uber() {
-
-	if (inFile.good()) {
-		while (!inFile.eof()) {
-			getline(inFile, myData);
-
-			if (myData.find("uber") < myData.length() || myData.find("Uber") < myData.length()) {
-				cout << myData << endl;
-			}
-		}
-		inFile.close();
-
-	};
-
-};
-
-void search() {
-
-	if (inFile.good()) {
-		while (!inFile.eof()) {
-			getline(inFile, myData);
-
-			if (myData.find(" " + userInput + " ") < myData.length()) {
-				cout << myData << endl;
-			}
-		}
-		inFile.close();
-
-	};
-
-};
-
+}
